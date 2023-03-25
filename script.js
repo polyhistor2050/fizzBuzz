f11ba37eb0da379961b388695f87082dd581030b
polyhistor2050 <chrisspaul2050@gmail.com>
2023-03-25 23:22:56 +0300

    add while loop inside the function

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ var output = [];
 var count = 1;
 
 function fizzBuzz() { 
-    if(count % 3 === 0 && count % 5 === 0) {
-        output.push("fizzbuzz");
-    }else if(count % 3 === 0){
-        output.push("fizz");
-    }else if(count % 5 === 0){
-        output.push("buzz");
-    }else {
-        output.push(count);
+    while(count <= 100){
+        if(count % 3 === 0 && count % 5 === 0) {
+            output.push("fizzbuzz");
+        }else if(count % 3 === 0){
+            output.push("fizz");
+        }else if(count % 5 === 0){
+            output.push("buzz");
+        }else {
+            output.push(count);
+        }
+        count++;
     }
-
     console.log(output);
-    count++;
+
 }
+fizzBuzz();
 
